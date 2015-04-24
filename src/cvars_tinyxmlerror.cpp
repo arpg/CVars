c60a329d4abebe4b91cb549368c33f00eab90dd4
crheckman <crheckman@gmail.com>
2015-04-24 09:43:50 -0600

    fixed CVars's poor directory structure

--- src/cvars_tinyxmlerror.cpp
+++ src/cvars_tinyxmlerror.cpp
@@ -22,7 +22,7 @@ must not be misrepresented as being the original software.
 distribution.
 */
 
-#include "cvars_tinyxml.h"
+#include <CVars/cvars_tinyxml.h>
 
 // The goal of the seperate error file is to make the first
 // step towards localization. tinyxml (currently) only supports
--- src/cvars_tinyxmlparser.cpp
+++ src/cvars_tinyxmlparser.cpp
@@ -25,7 +25,7 @@ distribution.
 #include <ctype.h>
 #include <stddef.h>
 
-#include "cvars_tinyxml.h"
+#include <CVars/cvars_tinyxml.h>
 
 //#define DEBUG_PARSER
 #if defined( DEBUG_PARSER )
